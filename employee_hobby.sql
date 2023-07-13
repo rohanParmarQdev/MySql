c0fad0e0641419f822ec0b54d4974325e73995ac
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-13 11:17:14 +0530

    MySql commit, employee_hobby v2

--- employee_hobby.sql
+++ employee_hobby.sql
@@ -5,3 +5,10 @@ create table employee_hobby
     fk_of_employee int unsigned not null,
     fk_hobby int unsigned not null
 );
+insert into employee_hobby values 
+(1,1,3),
+(2,1,1),
+(3,2,2),
+(4,2,1),
+(5,3,5);
+select * from employee_hobby;
